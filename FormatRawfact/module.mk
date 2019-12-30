b6bcdab0e793c6780e66558fa23b5e668cdec5f1
妖精梦梦 <1054550112@qq.com>
2019-12-30 19:04:10 +0800

    改一下makefile

--- FormatRawfact/module.mk
+++ FormatRawfact/module.mk
@@ -1,5 +1,5 @@
 local_src := $(wildcard $(subdirectory)/*.cpp)
 
-$(eval $(call make-program,FormatRawfact,libtriplebit.a,$(local_src)))
+$(eval $(call make-program,formatrawfact,libtriplebit.a,$(local_src)))
 
 $(eval $(call compile-rules))
